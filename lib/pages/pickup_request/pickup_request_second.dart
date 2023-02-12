de875105816e66d7fdf072510eb78085bf62b5b1
DarkFalc0n <unfezant211@gmail.com>
2023-02-12 15:50:26 +0530

    implemented order req

--- lib/pages/pickup_request/pickup_request_second.dart
+++ lib/pages/pickup_request/pickup_request_second.dart
@@ -1,9 +1,32 @@
-import 'package:ecell_nitb_hack2k23__generic_ftp/pages/confirm_request/confirm_request.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
 
 import '../../common/widgets/widgets.dart';
+import '../confirm_request/confirm_request.dart';
+
+Future<http.Response?> sendReq() async {
+  try {
+    String jsonData =
+        await rootBundle.loadString('lib/data/request_order.json');
+
+    var url = Uri.https('esummit-back.onrender.com', 'order/createorder');
+    var response = await http.post(
+      url,
+      body: jsonData,
+      headers: {
+        "Content-type": "application/json",
+      },
+    );
+    print(response.body);
+    return response;
+  } catch (err) {
+    print(err);
+    return null;
+  }
+}
 
 class PickupRequestSecondPage extends StatelessWidget {
   const PickupRequestSecondPage({Key? key}) : super(key: key);
@@ -100,13 +123,30 @@ class PickupRequestSecondPage extends StatelessWidget {
             StyledButton(
               height: 50.h,
               width: 200.w,
-              onClick: () => Navigator.pushAndRemoveUntil(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const ConfirmRequestPage(),
-                ),
-                (route) => false,
-              ),
+              onClick: () {
+                showDialog(
+                  barrierDismissible: false,
+                  context: context,
+                  builder: (context) => const AlertDialog(
+                    content: CircularProgressIndicator(
+                      color: Color(0xFFA7C957),
+                    ),
+                  ),
+                );
+                sendReq().then(
+                  (value) {
+                    if (value!.statusCode == 200) {
+                      Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const ConfirmRequestPage(),
+                        ),
+                        (_) => false,
+                      );
+                    }
+                  },
+                );
+              },
               child: Center(
                 child: Text(
                   'Confirm location',
